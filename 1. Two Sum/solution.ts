a53542951fa8d1e57db693d34a3043ad51431ac5
kuruxxii <zzzihanlin@gmail.com>
2024-03-10 11:19:28 +0800

    Create solution.ts

---
+++ 1. Two Sum/solution.ts
@@ -0,0 +1,10 @@
+function twoSum(nums: number[], target: number): number[] {
+  let map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    let complement = target - nums[i];
+    if (map.has(complement)) {
+      return [i, map.get(complement)];
+    }
+    map.set(nums[i], i);
+  }
+}
