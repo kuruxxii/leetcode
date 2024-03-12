1031db74dc91a8e97f9bf8d9d7d4cce672c9e5d8
Zihan Lin <93859201+kuruxxii@users.noreply.github.com>
2024-03-12 18:55:29 +0800

    Update solution.ts

--- 01. Two Sum/solution.ts
+++ 01. Two Sum/solution.ts
@@ -1,6 +1,7 @@
 /*
 The key idea behind this solution is that for each number nums[i] in the array,
-it checks if the complement target - nums[i] has already been encountered before.
+it checks if the complement: target - nums[i] has already been encountered before.
+Think about this case: target = 6, nums = [3, 3] to help understanding.
 If it has, then nums[i] and the previously encountered number sum up to the target.
 */
 
